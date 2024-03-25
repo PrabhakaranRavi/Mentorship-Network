20786024a13c2a5488d4663b981d5f737ca8b0d2
prabha-lumel <103419137+prabha-lumel@users.noreply.github.com>
2024-03-25 20:33:33 +0530

    Home page getting user details done

--- mentorship-network/src/Usermap.js
+++ mentorship-network/src/Usermap.js
@@ -1,15 +1,77 @@
 import "./Map.css";
+import "./styles.css";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import MarkerClusterGroup from "react-leaflet-cluster";
+import { Icon, divIcon, point } from "leaflet";
+import { createClient } from '@supabase/supabase-js';
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+// create custom icon
+const customIcon = new Icon({
+  iconUrl: require("./placeholder.png"),
+  iconSize: [38, 38]
+});
+
+// custom cluster icon
+const createClusterCustomIcon = function (cluster) {
+  return new divIcon({
+    html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
+    className: "custom-marker-cluster",
+    iconSize: point(33, 33, true)
+  });
+};
 
 export default function UserMap() {
-    return (
-        <MapContainer center={[9.939093, 78.121719]} zoom={10}>
-            {/* OPEN STREEN MAPS TILES */}
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-        </MapContainer>
-    );
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      let { data, error } = await supabase
+        .from('UserData')
+        .select('username, gmail, latitude, longitude, linkedin_url');
+
+      if (error) {
+        console.error('Error fetching data:', error);
+      } else {
+        setUserData(data);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <MapContainer center={[9.939093, 78.121719]} zoom={10}>
+      {/* OPEN STREET MAPS TILES */}
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <MarkerClusterGroup
+        chunkedLoading
+        iconCreateFunction={createClusterCustomIcon}
+      >
+        {/* Mapping through the fetched user data */}
+        {userData.map((user) => (
+          <Marker position={[user.latitude, user.longitude]} icon={customIcon}>
+            <Popup>
+              <div>
+                <h3>{user.username}</h3>
+               
+                   <a href={user.linkedin_url} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
+                
+              </div>
+            </Popup>
+          </Marker>
+        ))}
+      </MarkerClusterGroup>
+    </MapContainer>
+  );
 }
